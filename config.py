149ef5e3d3aef9c274f3e968ae37fbaad9ebf9c5
Abdulvohid <abdulvohidotamirzayeva@gmail.com>
2026-05-05 10:19:26 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,14 @@ import os
 # Your Telegram Bot Token from @BotFather
 BOT_TOKEN = "8640151765:AAE2Xg9m7Q4_RAhRrL4UgqEza2C_TM-pCzo"
 
-# Admin's Telegram ID
-ADMIN_ID = 6547348382  # Shu yerga o'zingizning Telegram ID raqamingizni yozing (masalan: 123456789)
-ADMIN_ID = 8235070957
-ADMIN_ID = 7964686556
-ADMIN_ID = 6471396425
+# Adminlar Telegram ID raqamlari
+ADMIN_IDS = [
+    6547348382,
+    8235070957,
+    7964686556,
+    6471396425
+]
+
 # List of mandatory channels
 # Format: {"id": "-100123456789", "url": "https://t.me/yourchannel", "name": "Kanal nomi"}
 MANDATORY_CHANNELS = [
@@ -17,7 +20,7 @@ MANDATORY_CHANNELS = [
          "url": "https://t.me/a21dev", 
          "name": "Kanal 1"
      },
-        {
+     {
          "id": "@kinoplay_4k", 
          "url": "https://t.me/kinoplay_4k", 
          "name": "Kanal 2"
